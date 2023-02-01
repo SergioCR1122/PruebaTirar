24bcbbbed38728cb7457103d9500058288abe015
SergioCR1122 <sergiomendieta11@gmail.com>
2023-02-01 22:29:23 +0100

    Agregadas variables

--- src/PruebaTirar.java
+++ src/PruebaTirar.java
@@ -4,6 +4,8 @@ public class PruebaTirar {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola alumnos");
+		
+		String nombre = "Sergio";
 	}
 
 }
